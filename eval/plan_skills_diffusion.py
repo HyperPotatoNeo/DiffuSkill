45abe3fd3bd175f4cddebb97c182e7233da0bde9
Shivesh Khaitan <shiveshkhaitan@gmail.com>
2023-04-08 18:09:12 -0400

    Fix eval

--- eval/plan_skills_diffusion.py
+++ eval/plan_skills_diffusion.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+import os
 
 import numpy as np
 import torch
@@ -16,10 +17,13 @@ from models.diffusion_models import (
 from models.skill_model import SkillModel
 
 
-def eval_func(envs,
+def eval_func(diffusion_model,
+              skill_model,
+              envs,
               state_dim,
               num_evals,
               num_parallel_envs,
+              num_diffusion_samples,
               extra_steps,
               planning_depth,
               render):
@@ -28,36 +32,36 @@ def eval_func(envs,
     num_evals = num_evals // num_parallel_envs
 
     for _ in range(num_evals):
-        state_0 = torch.zeros((num_parallel_envs, state_dim))
-        goal_state = torch.zeros((num_parallel_envs, 2))
+        state_0 = torch.zeros((num_parallel_envs, state_dim)).to(args.device)
+        goal_state = torch.zeros((num_parallel_envs, 2)).to(args.device)
 
-        for env_idx in envs:
-            state_0[env_idx] = torch.from_numpy(envs[env_idx].reset(), dtype=torch.float32)
-            goal_state[env_idx] = envs[env_idx].target_goal
+        for env_idx in range(len(envs)):
+            state_0[env_idx] = torch.from_numpy(envs[env_idx].reset())
+            goal_state[env_idx][0] = envs[env_idx].target_goal[0]
+            goal_state[env_idx][1] = envs[env_idx].target_goal[1]
 
         for env_step in range(1001):
             state = state_0.repeat_interleave(num_diffusion_samples, 0)
 
             latent_0 = diffusion_model.sample_extra(state, extra_steps=extra_steps)
-            state = self.decoder.abstract_dynamics(state, latent_0)
+            state, _ = skill_model.decoder.abstract_dynamics(state, latent_0)
 
-            for depth in range(planning_depth):
+            for depth in range(1, planning_depth):
                 latent = diffusion_model.sample_extra(state, extra_steps=extra_steps)
-                state = self.decoder.abstract_dynamics(state, latent)
+                state, _ = skill_model.decoder.abstract_dynamics(state, latent)
 
-            best_latent = torch.zeros((num_parallel_envs, latent_0.shape[1]))
+            best_latent = torch.zeros((num_parallel_envs, latent_0.shape[1])).to(args.device)
 
-            for env_idx in envs:
+            for env_idx in range(len(envs)):
                 start_idx = env_idx * num_diffusion_samples
                 end_idx = start_idx + num_diffusion_samples
 
-                cost = np.linalg.norm(state[start_idx : end_idx][:2] - goal_state[env_idx], axis=1)
+                cost = torch.linalg.norm(state[start_idx : end_idx][:, :2] - goal_state[env_idx], axis=1)
                 best_latent[env_idx] = latent_0[start_idx + torch.argmin(cost)]
 
-            action, _ = skill_model.decoder.ll_policy(state_0, best_latent)
-
-            for env_idx in envs:
-                new_state, _, _, _ = envs[env_idx].step(action.detach().cpu().numpy())
+            for env_idx in range(len(envs)):
+                action = skill_model.decoder.ll_policy.numpy_policy(state_0[env_idx], best_latent[env_idx])
+                new_state, _, _, _ = envs[env_idx].step(action)
                 state_0[env_idx] = torch.from_numpy(new_state)
 
             if render:
@@ -70,7 +74,7 @@ def evaluate(args):
     state_dim = dataset['observations'].shape[1]
     a_dim = dataset['actions'].shape[1]
 
-    checkpoint = torch.load(args.skill_model_path)
+    checkpoint = torch.load(os.path.join(args.checkpoint_dir, args.skill_model_filename))
 
     skill_model = SkillModel(state_dim,
                              a_dim,
@@ -87,26 +91,31 @@ def evaluate(args):
                              ).to(args.device)
 
     skill_model.load_state_dict(checkpoint['model_state_dict'])
+    skill_model.eval()
 
-    diffusion_nn_model = torch.load(args.diffusion_model_path).to(args.device)
+    diffusion_nn_model = torch.load(os.path.join(args.checkpoint_dir, args.skill_model_filename[:-4] + '_diffusion_prior.pt')).to(args.device)
 
     diffusion_model = Model_Cond_Diffusion(
         diffusion_nn_model,
         betas=(1e-4, 0.02),
         n_T=args.diffusion_steps,
-        device=device,
-        x_dim=diffusion_nn_model.xshape,
-        y_dim=diffusion_nn_model.y_dim,
+        device=args.device,
+        x_dim=state_dim,
+        y_dim=args.z_dim,
         drop_prob=None,
         guide_w=args.cfg_weight,
     )
+    diffusion_model.eval()
 
     envs = [gym.make(args.env) for _ in range(args.num_parallel_envs)]
 
-    eval_func(envs,
+    eval_func(diffusion_model,
+              skill_model,
+              envs,
               state_dim,
               args.num_evals,
               args.num_parallel_envs,
+              args.num_diffusion_samples,
               args.extra_steps,
               args.planning_depth,
               args.render,
@@ -122,10 +131,11 @@ if __name__ == "__main__":
     parser.add_argument('--num_evals', type=int, default=1)
     parser.add_argument('--num_parallel_envs', type=int, default=1)
     parser.add_argument('--skill_seq_len', type=int, default=1)
-    parser.add_argument('--skill_model_path', type=str)
-    parser.add_argument('--diffusion_model_path', type=str)
+    parser.add_argument('--checkpoint_dir', type=str, default='checkpoints')
+    parser.add_argument('--skill_model_filename', type=str)
+    parser.add_argument('--diffusion_model_filename', type=str)
 
-    parser.add_argument('--num_diffusion_samples', type=int, 50)
+    parser.add_argument('--num_diffusion_samples', type=int, default=50)
     parser.add_argument('--diffusion_steps', type=int, default=50)
     parser.add_argument('--cfg_weight', type=float, default=0.0)
     parser.add_argument('--planning_depth', type=int, default=3)
@@ -142,5 +152,7 @@ if __name__ == "__main__":
     parser.add_argument('--z_dim', type=int, default=256)
 
     parser.add_argument('--render', type=int, default=1)
+
+    args = parser.parse_args()
 
     evaluate(args)
