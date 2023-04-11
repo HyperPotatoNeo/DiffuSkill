17a277783c816ada41a438dfb688d5a4d676d5d7
Ravi <ravitej.akella@gmail.com>
2023-04-11 11:33:28 -0400

    Made minor changes to default hyperparams etc

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source activate cvideos
 
 # python training/train_skills.py --beta 0.1 --conditional_prior 0
 
-python training/train_skills.py --beta 0.01 --conditional_prior 0
+python training/train_skills.py --beta 1 --conditional_prior 0
 
 # python training/train_skills.py --beta 0.001 --conditional_prior 0
 
