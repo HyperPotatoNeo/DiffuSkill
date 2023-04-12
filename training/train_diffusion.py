41f4dd17c8934d1acb137c85d51e0e102b72ecfe
Shivesh Khaitan <shiveshkhaitan@gmail.com>
2023-04-12 19:37:29 -0400

    Remove normalization

--- training/train_diffusion.py
+++ training/train_diffusion.py
@@ -60,7 +60,7 @@ class PriorDataset(Dataset):
         if self.sample_z:
             latent_std = self.latent_all_std[index]
             latent = np.random.normal(latent,latent_std)
-            latent = (latent - self.latent_mean) / self.latent_std
+            # latent = (latent - self.latent_mean) / self.latent_std
 
         return (state, latent)
 
