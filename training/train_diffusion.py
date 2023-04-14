1a333e1bd48a9c35278204ec8b77af4342372e11
Shivesh Khaitan <shiveshkhaitan@gmail.com>
2023-04-14 00:31:02 -0400

    Fix latent std test set

--- training/train_diffusion.py
+++ training/train_diffusion.py
@@ -46,9 +46,11 @@ class PriorDataset(Dataset):
         if train_or_test == "train":
             self.state_all = self.state_all[:n_train]
             self.latent_all = self.latent_all[:n_train]
+            self.latent_all_std = self.latent_all_std[:n_train]
         elif train_or_test == "test":
             self.state_all = self.state_all[n_train:]
             self.latent_all = self.latent_all[n_train:]
+            self.latent_all_std = self.latent_all_std[n_train:]
         else:
             raise NotImplementedError
 
