bd4b2a839f0a14e7eb805d21b1be8235708d699c
HyperPotatoNeo <siddartv@andrew.cmu.edu>
2023-04-15 00:22:31 -0400

    Added DDQN model and training

---
+++ models/dqn.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.nn.utils import clip_grad_norm_
+import numpy as np
+import random
+from models.q_learning_models import MLP_Q
+from comet_ml import Experiment
+import copy
+from tqdm import tqdm
+
+class DDQN(nn.Module):
+    def __init__(self, state_dim, z_dim, h_dim=256, gamma=0.999, lr=1e-3, num_prior_samples=100, extra_steps=10, device='cuda', diffusion_prior=None):
+        super(DDQN,self).__init__()
+
+        self.state_dim = state_dim
+        self.z_dim = z_dim
+        self.gamma = gamma
+        self.lr = lr
+        self.num_prior_samples = num_prior_samples
+        self.extra_steps = extra_steps
+        self.device = device
+        self.tau = 1e-3
+        self.diffusion_prior = diffusion_prior
+
+        self.q_net_0 = MLP_Q(state_dim=state_dim,z_dim=z_dim,h_dim=h_dim).to(self.device)
+        self.q_net_1 = MLP_Q(state_dim=state_dim,z_dim=z_dim,h_dim=h_dim).to(self.device)
+        self.target_net_0 = None
+        self.target_net_1 = None
+        
+        self.optimizer_0 = optim.Adam(params=self.q_net_0.parameters(), lr=lr)
+        self.optimizer_1 = optim.Adam(params=self.q_net_1.parameters(), lr=lr)
+
+
+    @torch.no_grad()
+    def get_max_skills(self, states, net=0):
+        '''
+        INPUTS:
+            states: batch_size x state_dim
+        OUTPUTS:
+            max_z: batch_size x z_dim
+        '''
+        n_states = states.shape[0]
+        states = states.repeat_interleave(self.num_prior_samples, 0)
+        z_samples = self.diffusion_prior.sample_extra(states, predict_noise=0, extra_steps=self.extra_steps)
+        if net==0:
+            q_vals = self.q_net_0(states,z_samples)[:,0]
+        else:
+            q_vals = self.q_net_1(states,z_samples)[:,0]
+        q_vals = q_vals.reshape(n_states, self.num_prior_samples)
+        max_vals = torch.max(q_vals, dim=1)
+        max_q_vals = max_vals.values
+        max_indices = max_vals.indices
+        idx = torch.arange(n_states).cuda()*self.num_prior_samples + max_indices 
+        max_z = z_samples[idx]
+
+        return max_z, max_q_vals
+
+
+    def learn(self, dataload_train, dataload_test=None, n_epochs=10000, update_frequency=200, diffusion_model_name=''):
+        assert self.diffusion_prior is not None
+        experiment = Experiment(api_key = 'LVi0h2WLrDaeIC6ZVITGAvzyl', project_name = 'DiffuSkill')
+        experiment.log_parameters({'diffusion_prior':diffusion_model_name})
+        steps_net_0, steps_net_1, steps_total = 0, 0, 0
+        self.target_net_0 = copy.deepcopy(self.q_net_0)
+        self.target_net_1 = copy.deepcopy(self.q_net_1)
+        self.target_net_0.eval()
+        self.target_net_1.eval()
+        loss_net_0, loss_net_1, loss_total = 0, 0, 0 #Logged in comet at update frequency
+        epoch = 0
+
+        for ep in tqdm(range(n_epochs), desc="Epoch"):
+            n_batch = 0
+            loss_ep = 0
+            self.q_net_0.train()
+            self.q_net_1.train()
+            pbar = tqdm(dataload_train)
+
+            for s0,z,sT,reward in pbar:
+                s0 = s0.type(torch.FloatTensor).to(self.device)
+                z = z.type(torch.FloatTensor).to(self.device)
+                sT = sT.type(torch.FloatTensor).to(self.device)
+                reward = reward.type(torch.FloatTensor).to(self.device)
+                net_id = np.random.binomial(n=1, p=0.5, size=(1,))
+
+                if net_id==0:
+                    self.optimizer_0.zero_grad()
+
+                    q_s0z = self.q_net_0(s0,z)
+                    max_sT_skills,_ = self.get_max_skills(sT,net=1-net_id)
+                    q_sTz = self.target_net_0(sT,max_sT_skills.detach())
+                    q_target = reward + self.gamma*q_sTz
+                    
+                    bellman_loss = F.mse_loss(q_s0z, q_target)
+                    bellman_loss.backward()
+                    clip_grad_norm_(self.q_net_0.parameters(), 1)
+                    self.optimizer_0.step()
+                    loss_net_0 += bellman_loss.detach().item()
+                    loss_total += bellman_loss.detach().item()
+                    loss_ep += bellman_loss.detach().item()
+                    steps_net_0 += 1
+                else:
+                    self.optimizer_1.zero_grad()
+
+                    q_s0z = self.q_net_1(s0,z)
+                    max_sT_skills,_ = self.get_max_skills(sT,net=1-net_id)
+                    q_sTz = self.target_net_1(sT,max_sT_skills.detach())
+                    q_target = reward + self.gamma*q_sTz
+                    
+                    bellman_loss = F.mse_loss(q_s0z, q_target)
+                    bellman_loss.backward()
+                    clip_grad_norm_(self.q_net_1.parameters(), 1)
+                    self.optimizer_1.step()
+                    loss_net_1 += bellman_loss.detach().item()
+                    loss_total += bellman_loss.detach().item()
+                    loss_ep += bellman_loss.detach().item()
+                    steps_net_1 += 1
+
+                n_batch += 1
+                steps_total += 1
+                pbar.set_description(f"train loss: {loss_ep/n_batch:.4f}")
+
+                if steps_total%update_frequency == 0:
+                    loss_net_0 /= (steps_net_0+1e-4)
+                    loss_net_1 /= (steps_net_1+1e-4)
+                    loss_total /= update_frequency
+                    experiment.log_metric("train_loss_0", loss_net_0, step=steps_total)
+                    experiment.log_metric("train_loss_1", loss_net_1, step=steps_total)
+                    experiment.log_metric("train_loss", loss_total, step=steps_total)
+                    loss_net_0, loss_net_1, loss_total = 0,0,0
+                    steps_net_0, steps_net_1 == 0,0
+                    self.target_net_0 = copy.deepcopy(self.q_net_0)
+                    self.target_net_1 = copy.deepcopy(self.q_net_1)
+                    self.target_net_0.eval()
+                    self.target_net_1.eval()
+                    torch.save(self,  'q_checkpoints/'+diffusion_model_name+'_dqn_agent.pt')
+
+            experiment.log_metric("train_loss_episode", loss_ep/n_batch, step=epoch)
+            epoch += 1
---
+++ training/train_q_net.py
@@ -0,0 +1,127 @@
+from argparse import ArgumentParser
+import os
+from comet_ml import Experiment
+
+import d4rl
+import gym
+import pickle
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from models.diffusion_models import (
+    Model_mlp,
+    Model_cnn_mlp,
+    Model_Cond_Diffusion,
+)
+from models.dqn import DDQN
+
+class QLearningDataset(Dataset):
+    def __init__(
+        self, dataset_dir, filename, train_or_test="train", test_prop=0.1, sample_z=False
+    ):
+        # just load it all into RAM
+        self.state_all = np.load(os.path.join(dataset_dir, filename + "_states.npy"), allow_pickle=True)
+        self.latent_all = np.load(os.path.join(dataset_dir, filename + "_latents.npy"), allow_pickle=True)
+        self.sT_all = np.load(os.path.join(dataset_dir, filename + "_sT.npy"), allow_pickle=True)
+        self.rewards_all = (4*np.load(os.path.join(dataset_dir, filename + "_rewards.npy"), allow_pickle=True) - 30*4*0.5)/10 #zero-centering
+        self.sample_z = sample_z
+        if sample_z:
+            self.latent_all_std = np.load(os.path.join(dataset_dir, filename + "_latents_std.npy"), allow_pickle=True)
+        
+        n_train = int(self.state_all.shape[0] * (1 - test_prop))
+        if train_or_test == "train":
+            self.state_all = self.state_all[:n_train]
+            self.latent_all = self.latent_all[:n_train]
+            self.sT_all = self.sT_all[:n_train]
+            self.rewards_all = self.rewards_all[:n_train]
+        elif train_or_test == "test":
+            self.state_all = self.state_all[n_train:]
+            self.latent_all = self.latent_all[n_train:]
+            self.sT_all = self.sT_all[n_train:]
+            self.rewards_all = self.rewards_all[n_train:]
+        else:
+            raise NotImplementedError
+
+    def __len__(self):
+        return self.state_all.shape[0]
+
+    def __getitem__(self, index):
+        state = self.state_all[index]
+        latent = self.latent_all[index]
+        if self.sample_z:
+            latent_std = self.latent_all_std[index]
+            latent = np.random.normal(latent,latent_std)
+        sT = self.sT_all[index]
+        reward = self.rewards_all[index]
+
+        return (state, latent, sT, reward)
+
+
+def train(args):
+    env = gym.make(args.env)
+    dataset = env.get_dataset()
+    state_dim = dataset['observations'].shape[1]
+    a_dim = dataset['actions'].shape[1]
+
+    # get datasets set up
+    torch_data_train = QLearningDataset(
+        args.dataset_dir, args.skill_model_filename[:-4], train_or_test="train", test_prop=args.test_split, sample_z=args.sample_z
+    )
+    dataload_train = DataLoader(
+        torch_data_train, batch_size=args.batch_size, shuffle=True, num_workers=0
+    )
+    torch_data_test = QLearningDataset(
+        args.dataset_dir, args.skill_model_filename[:-4], train_or_test="test", test_prop=args.test_split, sample_z=args.sample_z
+    )
+    dataload_test = DataLoader(
+        torch_data_test, batch_size=args.batch_size, shuffle=True, num_workers=0
+    )
+
+    x_shape = torch_data_train.state_all.shape[1]
+    y_dim = torch_data_train.latent_all.shape[1]
+
+    # create model
+    diffusion_nn_model = torch.load(os.path.join(args.checkpoint_dir, args.skill_model_filename[:-4] + '_diffusion_prior_best.pt')).to(args.device)
+    model = Model_Cond_Diffusion(
+        diffusion_nn_model,
+        betas=(1e-4, 0.02),
+        n_T=args.diffusion_steps,
+        device=args.device,
+        x_dim=x_shape,
+        y_dim=y_dim,
+        drop_prob=args.drop_prob,
+        guide_w=0.0,
+    ).to(args.device)
+    model.eval()
+
+    dqn_agent = DDQN(state_dim = x_shape, z_dim=y_dim, diffusion_prior=model)
+    dqn_agent.learn(dataload_train=dataload_train, diffusion_model_name=args.skill_model_filename[:-4])
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument('--env', type=str, default='antmaze-large-diverse-v2')
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--n_epoch', type=int, default=10000)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--net_type', type=str, default='unet')
+    parser.add_argument('--n_hidden', type=int, default=512)
+    parser.add_argument('--test_split', type=float, default=0.1)
+    parser.add_argument('--sample_z', type=int, default=0)
+
+    parser.add_argument('--checkpoint_dir', type=str, default='checkpoints/')
+    parser.add_argument('--dataset_dir', type=str, default='data/')
+    parser.add_argument('--skill_model_filename', type=str)
+
+    parser.add_argument('--drop_prob', type=float, default=0.0)
+    parser.add_argument('--diffusion_steps', type=int, default=200)
+    parser.add_argument('--cfg_weight', type=float, default=0.0)
+    parser.add_argument('--predict_noise', type=int, default=0)
+
+    args = parser.parse_args()
+
+    train(args)
