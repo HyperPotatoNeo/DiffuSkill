92a558463842f710c2e2a0e7fe68b73142bc2398
Shivesh Khaitan <shiveshkhaitan@gmail.com>
2023-04-30 11:41:56 -0400

    Fix for append goals

--- training/collect_diffusion_data.py
+++ training/collect_diffusion_data.py
@@ -11,11 +11,13 @@ from utils.utils import get_dataset
 
 def collect_data(args):
     if 'antmaze' in args.env:
-        state_dim = 29
+        state_dim = 29 + args.append_goals * 2
         a_dim = 8
     elif 'kitchen' in args.env:
         state_dim = 60
         a_dim = 9
+    else:
+        raise NotImplementedError
 
     skill_model_path = os.path.join(args.checkpoint_dir, args.skill_model_filename)
 
@@ -50,7 +52,7 @@ def collect_data(args):
         batch_size=args.batch_size,
         num_workers=0)
 
-    states_gt = np.zeros((inputs_train.shape[0], state_dim+2*args.append_goals))
+    states_gt = np.zeros((inputs_train.shape[0], state_dim))
     latent_gt = np.zeros((inputs_train.shape[0], args.z_dim))
     if args.save_z_dist:
         latent_std_gt = np.zeros((inputs_train.shape[0], args.z_dim))
@@ -59,30 +61,22 @@ def collect_data(args):
     for batch_id, data in enumerate(train_loader):
         data = data.to(args.device)
         states = data[:, :, :skill_model.state_dim]
-        actions = data[:, :, skill_model.state_dim+2*args.append_goals:]
+        actions = data[:, :, skill_model.state_dim:]
 
         start_idx = batch_id * args.batch_size
         end_idx = start_idx + args.batch_size
-        states_gt[start_idx : end_idx] = data[:, 0, :skill_model.state_dim+2*args.append_goals].cpu().numpy()
+        states_gt[start_idx : end_idx] = data[:, 0, :skill_model.state_dim].cpu().numpy()
         sT_gt[start_idx: end_idx] = states[:, -1, :skill_model.state_dim].cpu().numpy()
         output, output_std = skill_model.encoder(states, actions)
         latent_gt[start_idx : end_idx] = output.detach().cpu().numpy().squeeze(1)
         if args.save_z_dist:
             latent_std_gt[start_idx : end_idx] = output_std.detach().cpu().numpy().squeeze(1)
 
-    if not args.append_goals:
-        np.save('data/' + args.skill_model_filename[:-4] + '_states.npy', states_gt)
-        np.save('data/' + args.skill_model_filename[:-4] + '_latents.npy', latent_gt)
-        np.save('data/' + args.skill_model_filename[:-4] + '_sT.npy', sT_gt)
-        if args.save_z_dist:
-            np.save('data/' + args.skill_model_filename[:-4] + '_latents_std.npy', latent_std_gt)
-    else:
-        np.save('data/' + args.skill_model_filename[:-4] + '_goals_states.npy', states_gt)
-        np.save('data/' + args.skill_model_filename[:-4] + '_goals_latents.npy', latent_gt)
-        np.save('data/' + args.skill_model_filename[:-4] + '_goals_sT.npy', sT_gt)
-        if args.save_z_dist:
-            np.save('data/' + args.skill_model_filename[:-4] + '_goals_latents_std.npy', latent_std_gt)
-
+    np.save('data/' + args.skill_model_filename[:-4] + '_states.npy', states_gt)
+    np.save('data/' + args.skill_model_filename[:-4] + '_latents.npy', latent_gt)
+    np.save('data/' + args.skill_model_filename[:-4] + '_sT.npy', sT_gt)
+    if args.save_z_dist:
+        np.save('data/' + args.skill_model_filename[:-4] + '_latents_std.npy', latent_std_gt)
 
 if __name__ == '__main__':
 
